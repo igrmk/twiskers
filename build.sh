efc6b2cc46634e2bff7ec1bdd60bab4bfa6e10b4
Igor Mikushkin <igor.mikushkin@gmail.com>
2025-02-26 23:15:20 +0000

    Fix macOS icon size

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ xmlstarlet ed -u "/_:svg/@viewBox" -v "-18.3 -18.3 292.6 292.6" src/twiskers-cat
 for i in 16 32 64 128 256 512 1024
 do
     inkscape -h $i "src/twiskers.svg" --export-area=-18.3:-18.3:274.3:274.3 --export-filename "icon/twiskers-${i}x${i}.png"
-    inkscape -h $i "src/twiskers.svg" --export-area=-20:-20:276:276 --export-filename "$mac_icons/twiskers-${i}x${i}.png"
+    inkscape -h $i "src/twiskers.svg" --export-area=-24:-24:280:280 --export-filename "$mac_icons/twiskers-${i}x${i}.png"
     exiftool -all= -overwrite_original_in_place "icon/twiskers-${i}x${i}.png"
     exiftool -all= -overwrite_original_in_place "$mac_icons/twiskers-${i}x${i}.png"
 done
